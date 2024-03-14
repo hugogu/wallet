8f5b887c45bfe537993d2f0b059dc674e0bcdb2c
Hugo Gu <hugogu@outlook.com>
2024-03-14 15:35:54 +0800

    将Kafka消息，通过Interceptor存到数据库。

--- wallet-service/build.gradle.kts
+++ wallet-service/build.gradle.kts
@@ -1,7 +1,12 @@
 dependencies {
     implementation(project(":data-model"))
+    implementation(project(":event-sourcing-kafka"))
+    implementation(project(":event-sourcing-mvc"))
     implementation("org.springframework.kafka:spring-kafka")
     implementation("org.springframework.cloud:spring-cloud-stream")
     implementation("org.springframework.cloud:spring-cloud-stream-binder-kafka")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.vladmihalcea:hibernate-types-52:2.16.2")
+    runtimeOnly("org.postgresql:postgresql")
 }
