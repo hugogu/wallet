0f9b138a855a774b8761f0328fad1ea573734503
Hugo Gu <hugogu@outlook.com>
2024-03-19 21:25:28 +0800

    Setup flink job

---
+++ wallet-flink/README.md
@@ -0,0 +1,5 @@
+# Wallet Flink Job
+
+
+## References
+* [Apache Flink quickstart with Kotlin and Gradle](https://mgaiduk.substack.com/p/apache-flink-quickstart-with-kotlin)
---
+++ wallet-flink/build.gradle.kts
@@ -0,0 +1,30 @@
+dependencies {
+    implementation(project(":common-tools"))
+    implementation(project(":model"))
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.apache.flink:flink-streaming-java:1.19.0")
+    implementation("org.apache.flink:flink-clients:1.19.0")
+    implementation("org.apache.flink:flink-connector-kafka:3.0.2-1.18")
+    implementation("org.springframework.kafka:spring-kafka")
+    runtimeOnly("org.postgresql:postgresql")
+}
+
+// Conflicts with Flink's log4j2 dependency
+configurations.implementation {
+    exclude(group = "ch.qos.logback", module = "*")
+}
+
+tasks.jar {
+    isZip64 = true
+    manifest.attributes["Main-Class"] = "FraudDetection"
+    val dependencies = configurations
+        .runtimeClasspath
+        .get()
+        .map(::zipTree)
+    from(dependencies)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    exclude("META-INF/*")
+    exclude("org/apache/flink/*")
+    exclude("org/scala-lang/*")
+}
+
